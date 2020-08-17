ac33aebd85de8e6d33e27e076dbdd709a5e0e986
ghong <ghd644@gmail.com>
2020-08-17 21:58:21 +0900

    모듈

---
+++ module.py
@@ -0,0 +1,13 @@
+# Price
+
+def price(people):
+    print("{0} people, price is {1}.".format(people, people*1000))
+
+# Morning discount
+def price_morning(people):
+    print("{0} people, morning discount price is {1}".format(people, people * 500))
+
+# Soldier Discount
+def price_soldier(people):
+    print("{0} people, Soldier discount price is {1}".format(people, people * 100))
+
---
+++ module_main.py
@@ -0,0 +1,24 @@
+# import module
+
+# module.price(3)
+# module.price_morning(4)
+# module.price_soldier(5)
+
+'''
+import module as mv
+mv.price(3)
+mv.price_morning(4)
+mv.price_soldier(5)
+'''
+
+''' 
+from module import *
+price(3)
+price_morning(4)
+price_soldier(5)
+'''
+
+from module import price, price_morning#, price_soldier
+price(3)
+price_morning(4)
+# price_soldier(5)  Can not use 
