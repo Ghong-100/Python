b166a6875d8701e1c1baf6d31a02bcd31b1cca31
ghong <ghd644@gmail.com>
2020-09-18 17:19:46 +0900

    링크드리스트 완성

--- Algorithm/DataStructure/03_LinkedList.py
+++ Algorithm/DataStructure/03_LinkedList.py
@@ -13,26 +13,47 @@ class LinkedList:
         self.tail = None
         self.size = 0
 
-    def add(self, data, index = 0):
+    def add(self, data, index = 0):  # 그냥 넣으면 맨 마지막에, 인덱스 넣으면 그 위치에
         # 인덱스가 사이즈보다 크면 불가능, 0보다 작으면 불가능
         if index > self.size or index < 0:
             print("인덱스 오류")
+        if self.head == None:
+            self.head = Node(data)
+            self.tail = self.head
+            self.size += 1
+        elif index == 0:
+            print("last")
+            newNode = Node(data)
+            self.tail.next = newNode
+            self.tail = self.tail.next
+            if self.size == 1:
+                self.head.next = self.tail
+            self.size += 1
         else:
             node = self.head
-            while index > 0:
+            cnt = 0
+            while cnt < index:
+                print(cnt)
                 node = node.next
-                index -= 1
-            newnode = Node(data)
-            newnode.next = node.next
-            node.next = newnode
+                cnt += 1
+            nextnode = node.next
+            node.next = Node(data)
+            node.next.next = nextnode
+            self.size += 1
+        
+        printnode = self.head
+        for i in range(0, self.size):
+            print(f"{i+1}번째 값 {printnode.data}")
+            printnode = printnode.next
+
 
     def remove(self, index):
         pass
     def printAll(self):
         curNode = self.head
         index = 0
-        while curNode != self.tail:
-            print(f"{index}. 값 : {curNode.data}")
+        while index < self.size:
+            print(f"{index+1}. 값 : {curNode.data}")
             curNode = curNode.next
             index += 1
 
