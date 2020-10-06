458cf5db0c57717b29e65f4d4bf5428a15fae644
ghong <ghd644@gmail.com>
2020-10-06 16:02:36 +0900

    YNA 완료

--- BeautifulSoup/Main.py
+++ BeautifulSoup/Main.py
@@ -11,7 +11,9 @@ class news:
     def __init__(self):
         self.m_title = ''       # [속보]김근홍 휴가쓰고싶어해. "연휴가 너무 짧다"
         self.m_time = ''        # %m-%d %H:%M
-        self.m_link = ''        # https://new.ghong.co.kr/latest?12345467
+        self.m_url = ''        # https://new.ghong.co.kr/latest?12345467
+    def print(self):
+        print(f"Time : {self.m_time}\nTitle : {self.m_title}\nURL : {self.m_url}")
 
 class site:
     def __init__(self, url):
@@ -21,10 +23,12 @@ class site:
         # print(self.m_url, self.domain)
         mod_name = str(self.domain)# + ".py"
         # print(mod_name)
-        mod = __import__('%s' %(mod_name), fromlist=[mod_name])
-        scrap = getattr(mod, 'scrap')
-        scrap(url)
-
+        self.m_module = __import__('%s' %(mod_name), fromlist=[mod_name])
+        self.scrap = getattr(self.m_module, 'scrap')
+        self.scrap_all = getattr(self.m_module, 'scrap_all')
+        # self.scrap_all(url)
+    def scrapAllNews(self):
+        return self.scrap_all(self.m_url)
 
 
 #############################################################################################
@@ -32,7 +36,8 @@ class site:
 #############################################################################################
 
 ## Read files
-site_list = []
+Site_list = []
+News_list = []
 with open("D:\\30_STUDY\\python\\BeautifulSoup\\site\\sites.txt", "r", encoding="utf8") as data:    
     '''
     while True:
@@ -42,6 +47,11 @@ with open("D:\\30_STUDY\\python\\BeautifulSoup\\site\\sites.txt", "r", encoding=
         site_list.append(site(line))
     '''
     line = data.readline()
-    site_list.append(site(line))
+    if line != None: 
+        newSite = site(line)
+        Site_list.append(newSite)
+        for news in newSite.scrapAllNews():
+            News_list.append(news)
+
 
 print("end")
--- BeautifulSoup/site/yna.py
+++ BeautifulSoup/site/yna.py
@@ -1,30 +1,114 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import time
+from datetime import datetime
+
+class news:
+    def __init__(self):
+        self.m_title = ''       # [속보]김근홍 휴가쓰고싶어해. "연휴가 너무 짧다"
+        self.m_time = ''        # %m-%d %H:%M
+        self.m_url = ''        # https://new.ghong.co.kr/latest?12345467
+    def print(self):
+        print(f"Time : {self.m_time}\nTitle : {self.m_title}\nURL : {self.m_url}")
 
 def check():
     print(__name__)
 
 def scrap(url):
-    print(url + '스크랩을 시작!!')
-
     html = urlopen(url)
     bsObject = BeautifulSoup(html, "html.parser")
-    print(type(bsObject))
+
+    newsList = []
+    currTime = datetime.now().strftime('%Y-%m-%d %H:%M')
 
     with open("BeautifulSoup\\Site\\yna_find.html","w", encoding="utf8") as output:
         bsFind = bsObject.find('section')#, attrs={'class':'tit-news'})
-        print(type(bsFind))
         findData = bsFind.find_all(True, {"class": ["txt-time","tit-wrap","tit-news"]})
         #bsFind = bsFind.find_all(True, {"class": ["txt-time","tit-news"]})
+        count = 0
+        index = 0   # 1 = 시간, 2 = url, 3 = 뉴스제목
+        skip = False
         for line in findData:
+            index = index + 1 if index + 1 <= 3 else 1
+            if index == 1:  # 시간 처리
+                newsTime = str(line.get_text())
+                newsTime = datetime.now().strftime('%Y') + '-' + newsTime
+                if newsTime != None and newsTime == currTime:
+                    newNews = news()
+                    newNews.m_time = newsTime
+                    newsList.append(newNews)
+                    count += 1
+                    skip = False
+                else:
+                    skip = True
+            elif index == 2: # url 처리
+                if skip == True:
+                    continue
+                if 'href' in line.attrs:
+                    newsList[count-1].m_url = "https:" + str(line.attrs['href'])
+            elif index == 3: # 뉴스 제목
+                if skip == True:
+                    continue
+                newsTitle = str(line.get_text())
+                if newsTitle != None:
+                    newsList[count-1].m_title = newsTitle
+                    newsList[count-1].print()
+        return newsList
+
+def scrap_all(url):
+    html = urlopen(url)
+    bsObject = BeautifulSoup(html, "html.parser")
+    newsList = []
+
+    with open("BeautifulSoup\\Site\\yna_find.html","w", encoding="utf8") as output:
+        bsFind = bsObject.find('section')
+        findData = bsFind.find_all(True, {"class": ["txt-time","tit-wrap","tit-news"]})
+        count = 0
+        index = 0   # 1 = 시간, 2 = url, 3 = 뉴스제목
+        skip = False
+        for line in findData:
+            index = index + 1 if index + 1 <= 3 else 1
+            if index == 1:  # 시간 처리
+                newsTime = str(line.get_text())
+                newsTime = datetime.now().strftime('%Y') + '-' + newsTime
+                if newsTime != None:
+                    newNews = news()
+                    newNews.m_time = newsTime
+                    newsList.append(newNews)
+                    count += 1
+                    skip = False
+                else:
+                    skip = True
+            elif index == 2: # url 처리
+                if skip == True:
+                    continue
+                if 'href' in line.attrs:
+                    newsList[count-1].m_url = "https:" + str(line.attrs['href'])
+            elif index == 3: # 뉴스 제목
+                if skip == True:
+                    continue
+                newsTitle = str(line.get_text())
+                if newsTitle != None:
+                    newsList[count-1].m_title = newsTitle
+                    newsList[count-1].print()
+                    output.write(newsList[count-1].m_time + '\n')
+                    output.write(newsList[count-1].m_title + '\n')
+                    output.write(newsList[count-1].m_url + '\n\n')
+        return newsList
+
+
+
+
+
+'''
             if 'href' in line.attrs:
                 writedata = str(line.attrs['href'])
                 output.write(writedata+'\n')
             else:
                 writedata = str(line.get_text())
                 output.write(writedata+'\n')
-            print("One line end!!!!")
+'''
+
         # while True:
         #     findData = bsFind.find('span', attrs={'class':'txt-time'})
         #     if findData == None:
