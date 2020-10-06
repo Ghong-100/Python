44e73d5ab02d5c23f082caef793df0c8afd1d6a9
ghong <ghd644@gmail.com>
2020-10-06 14:08:34 +0900

    뉴스 사이트 크롤링

--- BeautifulSoup/Main.py
+++ BeautifulSoup/Main.py
@@ -1,20 +1,30 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import os
-import re
-import inspect
+import os, re
+
+import sys, inspect
+sys.path.append('BeautifulSoup\\site')
 
 repattern = r"^(http(s)?:\/\/)(www\.|m\.)?([a-z0-9]+)(\.*)"
+
+class news:
+    def __init__(self):
+        self.m_title = ''       # [속보]김근홍 휴가쓰고싶어해. "연휴가 너무 짧다"
+        self.m_time = ''        # %m-%d %H:%M
+        self.m_link = ''        # https://new.ghong.co.kr/latest?12345467
 
 class site:
     def __init__(self, url):
         self.m_url = url
         res = re.match(repattern, url)
         self.domain = res.group(4)
-        print(self.m_url, self.domain)
-        mod_name = str(self.domain) + ".py"
-        print(mod_name)
+        # print(self.m_url, self.domain)
+        mod_name = str(self.domain)# + ".py"
+        # print(mod_name)
         mod = __import__('%s' %(mod_name), fromlist=[mod_name])
+        scrap = getattr(mod, 'scrap')
+        scrap(url)
+
 
 
 #############################################################################################
@@ -23,11 +33,15 @@ class site:
 
 ## Read files
 site_list = []
-with open("D:\\30_STUDY\\python\\BeautifulSoup\\site\\sites.txt", "r", encoding="utf8") as data:
+with open("D:\\30_STUDY\\python\\BeautifulSoup\\site\\sites.txt", "r", encoding="utf8") as data:    
+    '''
     while True:
         line = data.readline()
         if not line:
             break
         site_list.append(site(line))
+    '''
+    line = data.readline()
+    site_list.append(site(line))
 
 print("end")
--- BeautifulSoup/site/yna.py
+++ BeautifulSoup/site/yna.py
@@ -1,2 +1,38 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import time
+
+def check():
+    print(__name__)
+
 def scrap(url):
-    print(url)
+    print(url + '스크랩을 시작!!')
+
+    html = urlopen(url)
+    bsObject = BeautifulSoup(html, "html.parser")
+    print(type(bsObject))
+
+    with open("BeautifulSoup\\Site\\yna_find.html","w", encoding="utf8") as output:
+        bsFind = bsObject.find('section')#, attrs={'class':'tit-news'})
+        print(type(bsFind))
+        findData = bsFind.find_all(True, {"class": ["txt-time","tit-wrap","tit-news"]})
+        #bsFind = bsFind.find_all(True, {"class": ["txt-time","tit-news"]})
+        for line in findData:
+            if 'href' in line.attrs:
+                writedata = str(line.attrs['href'])
+                output.write(writedata+'\n')
+            else:
+                writedata = str(line.get_text())
+                output.write(writedata+'\n')
+            print("One line end!!!!")
+        # while True:
+        #     findData = bsFind.find('span', attrs={'class':'txt-time'})
+        #     if findData == None:
+        #         break
+        
+
+        #bsFind = bsObject.find('strong', {'class':'tit-news'}).get_text()
+        #output.write(str(bsObject))
+
+        # class="tit-news"
+        # class="txt-time"
