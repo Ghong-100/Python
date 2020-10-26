f2d55216d55e7cd74fff80ce7c2ef9060b6cfbd2
ghong <ghd644@gmail.com>
2020-10-26 14:43:11 +0900

    재귀 문제풀이, 동적프로그래밍

--- Algorithm/Algorithm/13_RecursiveCall.py
+++ Algorithm/Algorithm/13_RecursiveCall.py
@@ -5,7 +5,7 @@ def factorial(n):
     else:
         return 1
 
-print(factorial(10))
+# print(factorial(10))
 
 def multiple(data):
     if data <= 1:
@@ -21,11 +21,54 @@ def sum_list(data):
     else:
         return data.pop() + sum_list(data)
 
-data = []
+# data = []
+# for i in range(10):
+#     data.append(random.randint(1,100))
+# print(data)
+# print(sum_list(data))
 
-for i in range(10):
-    data.append(random.randint(1,100))
-print(data)
-print(sum_list(data))
+# print(multiple(10))
 
-print(multiple(10))
+def isPalindrome(data, index = 0):
+    length = len(data)
+    if index > length/2:
+        return True
+    elif data[index] == data[-(index+1)]:
+        return isPalindrome(data, index+1)
+    else:
+        return False
+
+# tes = 'ghonggnohg'
+# print(isPalindrome(tes))
+
+
+# 숫자가 주어졌을때 홀수면 3*n+1을 하고 짝수면 n/2를 해서 1이 나올때까지 결과 출력
+def Test1(num):
+    print(num)
+    if num == 1:
+        return
+    if num%2 == 1:
+        Test1(3*num+1)
+    else:
+        Test1(int(num/2))
+
+# Test1(99999)
+
+# 숫자가 주어졌을때 1,2,3의 합으로 나타낼 수 있는 경우의 수
+# 4가 주어지면
+# 1+1+1+1
+# 1+1+2
+# 1+2+1
+# 1+3
+# 2+1+1
+# 2+2
+# 3+1
+# 총 7가지
+def Test2(num):
+    if num <= 3:
+        return 2**(num-1)
+    else:
+        return Test2(num-1) + Test2(num-2) + Test2(num-3)
+
+
+print(Test2(6))
