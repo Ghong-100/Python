b73052f7bfe5ea03d34b0b1287f9525909006e16
ghong <ghd644@gmail.com>
2020-11-23 21:00:08 +0900

    집 변경내용 동기화

---
+++ Algorithm/1_array.py
@@ -0,0 +1,54 @@
+# 파이썬은 리스트로 지원함
+
+data = []
+for num in range(1,6):
+     data.insert(num-1, num)
+
+print(data)
+
+# 2차원 배열
+data = [[1,2,3],[4,5],[6,7,8]]
+print(data)
+for ran in range(0,len(data)):
+    print(data[ran])
+
+# 빈도수 출력 프로그램
+dataset = ['Braund, Mr. Owen Harris',
+'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',
+'Heikkinen, Miss. Laina',
+'Futrelle, Mrs. Jacques Heath (Lily May Peel)',
+'Allen, Mr. William Henry',
+'Moran, Mr. James',
+'McCarthy, Mr. Timothy J',
+'Palsson, Master. Gosta Leonard',
+'Johnson, Mrs. Oscar W (Elisabeth Vilhelmina Berg)',
+'Nasser, Mrs. Nicholas (Adele Achem)',
+'Sandstrom, Miss. Marguerite Rut',
+'Bonnell, Miss. Elizabeth',
+'Saundercock, Mr. William Henry',
+'Andersson, Mr. Anders Johan',
+'Vestrom, Miss. Hulda Amanda Adolfina',
+'Hewlett, Mrs. (Mary D Kingcome) ',
+'Rice, Master. Eugene',
+'Williams, Mr. Charles Eugene',
+'Vander Planke, Mrs. Julius (Emelia Maria Vandemoortele)',
+'Masselmani, Mrs. Fatima',
+'Fynney, Mr. Joseph J',
+'Beesley, Mr. Lawrence',
+'McGowan, Miss. Anna "Annie"',
+'Sloper, Mr. William Thompson',
+'Palsson, Miss. Torborg Danira',
+'Asplund, Mrs. Carl Oscar (Selma Augusta Emilia Johansson)',
+'Emir, Mr. Farred Chehab',
+'Fortune, Mr. Charles Alexander',
+'Dwyer, Miss. Ellen "Nellie"',
+'Todoroff, Mr. Lalio']
+
+cnt = 0
+for data in dataset:
+    for index in range(len(data)):
+        if data[index] == 'M':
+            cnt += 1
+
+
+print(cnt)
